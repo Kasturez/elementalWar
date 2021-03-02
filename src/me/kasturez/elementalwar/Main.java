46e04394b5f60aad6a50866fe5c60f48dcba6b72
BuildTools <unconfigured@null.spigotmc.org>
2021-03-02 17:13:07 +0700

    fixed

--- src/me/kasturez/elementalwar/Main.java
+++ src/me/kasturez/elementalwar/Main.java
@@ -10,13 +10,11 @@ public class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        //Fired when the server enables the plugin
         getLogger().info("onEnable has been called");
     }
 
     @Override
     public void onDisable() {
-        //Fired when the server stops and disables all plugins
         getLogger().info("onDisable has been called");
     }
 
@@ -32,22 +30,6 @@ public class Main extends JavaPlugin {
                 sender.sendMessage(ChatColor.RED + "you are a player");
                 return true;
             }
-        }
-        //set element
-        if (command.getName().equalsIgnoreCase("launch")) {
-            if (!(sender instanceof Player)) {
-                sender.sendMessage("*console goes flying*");
-                return true;
-            }
-            Player player = (Player) sender;
-            if (args.length == 0) {
-                sender.sendMessage(ChatColor.BLACK + "" + ChatColor.BOLD + "ZOOOOOOOOM!");
-                player.setVelocity(player.getLocation().getDirection().multiply(2).setY(2));
-                return true;
-            }
-            sender.sendMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "ZOOOOOOOOM!");
-            player.setVelocity(player.getLocation().getDirection().multiply(Integer.parseInt(args[0])).setY(2));
-            return true;
         }
         return false;
     }
