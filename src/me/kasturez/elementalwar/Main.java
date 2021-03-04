70afbd0860252b88bf941825ac96efac1628b023
BuildTools <unconfigured@null.spigotmc.org>
2021-03-04 17:37:33 +0700

    commit

--- src/me/kasturez/elementalwar/Main.java
+++ src/me/kasturez/elementalwar/Main.java
@@ -1,6 +1,6 @@
 package me.kasturez.elementalwar;
 
-import com.google.gson.internal.LinkedTreeMap;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -8,14 +8,14 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.event.entity.ItemMergeEvent;
-import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +25,7 @@ public class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         openkits();
+
     }
 
     @Override
@@ -34,106 +35,129 @@ public class Main extends JavaPlugin {
 
     private static Inventory kits;
     private void openkits() {
-        Inventory inv = Bukkit.createInventory(null, 18, ChatColor.BOLD + "Kits" + ChatColor.GOLD);
+        Inventory inv = Bukkit.createInventory(null, 18, ChatColor.AQUA + "Kits");
         ItemStack item = new ItemStack(Material.BREAD);
         ItemMeta meta = item.getItemMeta();
         assert meta != null;
-        meta.setDisplayName(ChatColor.BLACK + "Food");
+        meta.setDisplayName(ChatColor.DARK_GRAY + "Food");
         List<String> lore = new ArrayList<>();
         lore.add("");
         lore.add(ChatColor.LIGHT_PURPLE + "Begin the journey!");
+        meta.setLore(lore);
         meta.addEnchant(Enchantment.DURABILITY, 1, true);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
         item.setItemMeta(meta);
         inv.setItem(0, item);
+        lore.clear();
+
 
         item.setType(Material.WOODEN_SWORD);
         meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.GRAY + "Traveler");
+        meta.setDisplayName(ChatColor.DARK_GRAY + "Traveler");
         lore.add("");
         lore.add(ChatColor.LIGHT_PURPLE + "Begin the journey");
         meta.addEnchant(Enchantment.DURABILITY, 1, true);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         item.setItemMeta(meta);
         inv.setItem(1, item);
+        lore.clear();
 
         item.setType(Material.STONE_SWORD);
         meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.BLACK + "Low tier soldier");
+        meta.setDisplayName(ChatColor.DARK_GREEN + "Low tier soldier");
         lore.add("");
         lore.add(ChatColor.LIGHT_PURPLE + "Shabby but this will do");
+        meta.setLore(lore);
         meta.addEnchant(Enchantment.DURABILITY, 1, true);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         item.setItemMeta(meta);
         inv.setItem(2, item);
+        lore.clear();
 
-        meta.setDisplayName(ChatColor.BLACK + "Mid tier soldier");
+        meta.setDisplayName(ChatColor.DARK_GREEN + "Mid tier soldier");
         lore.add("");
         lore.add(ChatColor.LIGHT_PURPLE + "Better but not there yet");
+        meta.setLore(lore);
         meta.addEnchant(Enchantment.DURABILITY, 1, true);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         item.setItemMeta(meta);
         inv.setItem(3, item);
+        lore.clear();
 
-        meta.setDisplayName(ChatColor.BLACK + "Top tier soldier");
+        meta.setDisplayName(ChatColor.DARK_GREEN + "Top tier soldier");
         lore.add("");
         lore.add(ChatColor.LIGHT_PURPLE + "One of the best set for a soldier");
+        meta.setLore(lore);
         meta.addEnchant(Enchantment.DURABILITY, 1, true);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         item.setItemMeta(meta);
         inv.setItem(4, item);
+        lore.clear();
 
         item.setType(Material.IRON_SWORD);
         meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.BLACK + "Low tier warrior");
+        meta.setDisplayName(ChatColor.DARK_AQUA + "Low tier warrior");
+        meta.setLore(lore);
         lore.add("");
         lore.add(ChatColor.LIGHT_PURPLE + "Average set for a warrior path");
         meta.addEnchant(Enchantment.DURABILITY, 1, true);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         item.setItemMeta(meta);
         inv.setItem(5, item);
+        lore.clear();
 
-        meta.setDisplayName(ChatColor.BLACK + "Mid tier warrior");
+        meta.setDisplayName(ChatColor.DARK_AQUA + "Mid tier warrior");
         lore.add("");
         lore.add(ChatColor.LIGHT_PURPLE + "A solid set for a brave warrior");
+        meta.setLore(lore);
         meta.addEnchant(Enchantment.DURABILITY, 1, true);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         item.setItemMeta(meta);
         inv.setItem(6, item);
+        lore.clear();
 
-        meta.setDisplayName(ChatColor.BLACK + "Top tier warrior");
+        meta.setDisplayName(ChatColor.DARK_AQUA + "Top tier warrior");
         lore.add("");
-        lore.add(ChatColor.LIGHT_PURPLE + "a step into the godly set realm");
+        lore.add(ChatColor.LIGHT_PURPLE + "A step into the godly set realm");
+        meta.setLore(lore);
         meta.addEnchant(Enchantment.DURABILITY, 1, true);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         item.setItemMeta(meta);
         inv.setItem(7, item);
+        lore.clear();
 
         item.setType(Material.DIAMOND_SWORD);
         meta = item.getItemMeta();
-        meta.setDisplayName(ChatColor.BLACK + "Low tier warlord");
+        meta.setDisplayName(ChatColor.DARK_RED + "Low tier warlord");
         lore.add("");
-        lore.add(ChatColor.LIGHT_PURPLE + "there is a faint trace of divinity upon this set");
+        lore.add(ChatColor.LIGHT_PURPLE + "There is a faint trace of divinity upon this set");
+        meta.setLore(lore);
         meta.addEnchant(Enchantment.DURABILITY, 1, true);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         item.setItemMeta(meta);
         inv.setItem(8, item);
+        lore.clear();
 
-        meta.setDisplayName(ChatColor.BLACK + "Mid tier warlord");
+        meta.setDisplayName(ChatColor.DARK_RED + "Mid tier warlord");
         lore.add("");
-        lore.add(ChatColor.LIGHT_PURPLE + "a flawless set from the finest craftsmanship upon the star");
+        lore.add(ChatColor.LIGHT_PURPLE + "A flawless set from the finest craftsmanship upon the star");
+        meta.setLore(lore);
         meta.addEnchant(Enchantment.DURABILITY, 1, true);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         item.setItemMeta(meta);
         inv.setItem(9, item);
+        lore.clear();
 
-        meta.setDisplayName(ChatColor.BLACK + "Top tier warlord");
+        meta.setDisplayName(ChatColor.DARK_RED + "Top tier warlord");
         lore.add("");
         lore.add(ChatColor.LIGHT_PURPLE + "A mystical set with radiant amount of divinity");
+        meta.setLore(lore);
         meta.addEnchant(Enchantment.DURABILITY, 1, true);
         meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         item.setItemMeta(meta);
         inv.setItem(10, item);
+        lore.clear();
 
         kits = inv;
     }
@@ -149,5 +173,34 @@ public class Main extends JavaPlugin {
             player.openInventory(kits);
         }
         return false;
+    }
+    @EventHandler
+    public void onClick(InventoryClickEvent event){
+        if(!event.getView().getTitle().contains("Kits"))
+            return;
+        if(event.getCurrentItem() == null)
+            return;
+        if(event.getCurrentItem().getItemMeta() == null)
+            return;
+
+        Player player = (Player) event.getWhoClicked();
+        event.setCancelled(true);
+
+        /*if(event.getClickedInventory().getType() == InventoryType.PLAYER)
+            return;*/
+
+        //slot of inventory kits
+        if(event.getSlot() == 0) {
+            //Food kit
+            if (!player.hasPermission("kits.Food")) {
+                player.sendMessage("You are not allow to open this kit");
+                return;
+            }
+            
+            //drop chest of kits
+            player.closeInventory();
+            player.updateInventory();
+
+        }
     }
 }
