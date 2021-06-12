5741388daf97cba43808ad0277801eb7d78031da
BuildTools <unconfigured@null.spigotmc.org>
2021-06-12 11:46:12 +0700

    update Main.java and plugin.yml

--- src/me/kasturez/elementalwar/Main.java
+++ src/me/kasturez/elementalwar/Main.java
@@ -1,6 +1,8 @@
 package me.kasturez.elementalwar;
 
 
+import me.kasturez.elementalwar.guild.commands.DamageEvent;
+import me.kasturez.elementalwar.guild.commands.FlyCMD;
 import me.kasturez.elementalwar.guild.commands.GuildCMD;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,6 +13,8 @@ public class Main extends JavaPlugin implements CommandExecutor {
     @Override
     public void onEnable() {
         getCommand("guild").setExecutor(new GuildCMD(this));
+        getCommand("fly").setExecutor(new FlyCMD());
+        getServer().getPluginManager().registerEvents(new DamageEvent(), this);
     }
 
     @Override
